21c2d5fd1a22b3762c64b167d1ec7d057136084c
iartemyev <i.s.artemyev@hotmail.com>
2022-04-06 15:41:43 +0300

    wip

--- frontend/sign-up/src/app/content/signup/step-first/signup-step-first.module.ts
+++ frontend/sign-up/src/app/content/signup/step-first/signup-step-first.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupStepFirstComponent } from './signup-step-first.component';
 import { SignupStepFirstRoutingModule } from './signup-step-first-routing.module';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	declarations: [SignupStepFirstComponent],
-	imports: [CommonModule, SignupStepFirstRoutingModule],
+	imports: [
+		CommonModule,
+		SignupStepFirstRoutingModule,
+		FormsModule,
+		MatCardModule,
+		MatInputModule,
+		MatButtonModule,
+	],
 })
 export class SignupStepFirstModule {}
